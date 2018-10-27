e04c383ca340d94b58f6d2daad6bf76f8e14d90a
Rageeb Mahtab <rmahtab0079@bths.edu>
2018-10-27 15:31:49 +0000

    Last commits

--- app.js
+++ app.js
@@ -8,17 +8,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
-var Persons = [
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
-];
+
 
 var newSignUpSchema = new mongoose.Schema({ 
     Name : String,
@@ -145,6 +135,10 @@ app.get("/new_volunteer", function(req, res){
 
 app.get("/Checkout", function(req, res){
     res.render("Checkout");
+});
+
+app.get("/shop-list", function(req, res){
+    res.render("shop-list");
 });
 
 
