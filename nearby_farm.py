459293af1c2806e98d663d9114c32dbaf75e76b3
Marlon Pimentel <marlon97pimentel@hotmail.com>
2018-10-27 04:48:23 -0400

    completed closest farm algo

--- nearby_farm.py
+++ nearby_farm.py
@@ -7,13 +7,19 @@ farm_locations = {'Bay View': '42.779590+-78.857390',
                     'Forest': '40.833360+-73.907110',
                     'Mariner’s Harbor': '40.631080+-74.163650'}
 
-distances = {}
+closest_farm = ""
+closest_distance = 100000.0
 for farm in farm_locations:
     #  hardcoded Jersey office as origin to represent current location for now
-    r = requests.get("https://maps.googleapis.com/maps/api/directions/json?origin={}&destination=Universal+Studios+Hollywood&key=AIzaSyDxIMQRo1wJMCqp_QA-GbcwqhQjhLTPdpM".format(farm_locations[farm]))
+    r = requests.get("https://maps.googleapis.com/maps/api/directions/json?origin=40.728809+-74.035400&destination={}&key=AIzaSyDxIMQRo1wJMCqp_QA-GbcwqhQjhLTPdpM".format(farm_locations[farm]))
 
-    distance = r.json()['routes'][0]['legs'][0]['duration']['text']
-    distances[farm] = distance
+    distance = r.json()['routes'][0]['legs'][0]['distance']['text']
 
-for each in distances:
-    print(each, distance)
+    str_length = len(distance)
+    distance_revised = float(distance[:str_length - 2].strip())
+
+    if distance_revised < closest_distance:
+        closest_distance = distance_revised
+        closest_farm = farm
+
+print("{} Farm is the closest to you: {} miles away".format(closest_farm, closest_distance))
